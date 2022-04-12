814c47e27aee037c717981739cbb1b4d9783a197
Subhra's <subhrajyoti.mall@praxis.ac.in>
2022-04-12 13:52:15 +0530

    Q3.1

---
+++ Q3_1.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def base_route():
+    return "Enter your name in URL with leading /"
+
+@app.route("/<name>")
+def print_name(name):
+    return f"Welcome {name}" 
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port = 8685)
